b083c7a28bedfb32b20534d56b7d37c56d819df9
gonyango <grantononyango@gmail.com>
2024-12-20 21:16:26 +0300

    blueprint for the user db from user class that inherits from abstractUser class

--- main/models.py
+++ main/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
+#AbstractUser acts as a "template" for creating your own type of user in Django
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 ROLES = [
-    ('CUSTOMER', 'Customer'),
+    #('CUSTOMER', 'Customer'),
     ('AIR_CONDITIONER', 'Air Conditioner'),
     ('ALL_IN_ONE', 'All in One'),
     ('CARPENTRY', 'Carpentry'),
@@ -16,3 +20,16 @@ ROLES = [
     ('PLUMBING', 'Plumbing'),
     ('WATER_HEATERS', 'Water Heaters'),
 ]
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    role = models.CharField(max_length=100, choices=ROLES, default='None')
+    dob = models.DateField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    ##string identifier of the class
+    def __str__(self):
+        return self.email
