4ec625eb367a310a3e514cb045a4295e3ded43a4
gonyango <grantononyango@gmail.com>
2024-12-20 20:19:01 +0300

    feat: add profile path to users.urls

--- netfix/urls.py
+++ netfix/urls.py
@@ -23,7 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('services/', include('services.urls')),
-    path('register/', include('users.urls')),
-    path('customer/<slug:name>', v.customer_profile, name='customer_profile'),
-    path('company/<slug:name>', v.company_profile, name='company_profile')
+    path('profile/', include('users.urls')),
 ]
