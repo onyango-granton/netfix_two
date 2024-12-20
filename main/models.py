95425d811c3d44f71b485de62d83053413ff8488
gonyango <grantononyango@gmail.com>
2024-12-20 20:55:19 +0300

    feat: add possible work roles

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+ROLES = [
+    ('CUSTOMER', 'Customer'),
+    ('AIR_CONDITIONER', 'Air Conditioner'),
+    ('ALL_IN_ONE', 'All in One'),
+    ('CARPENTRY', 'Carpentry'),
+    ('ELECTRICITY', 'Electricity'),
+    ('GARDENING', 'Gardening'),
+    ('HOME_MACHINES', 'Home Machines'),
+    ('HOUSEKEEPING', 'Housekeeping'),
+    ('INTERIOR_DESIGN', 'Interior Design'),
+    ('LOCKS', 'Locks'),
+    ('PAINTING', 'Painting'),
+    ('PLUMBING', 'Plumbing'),
+    ('WATER_HEATERS', 'Water Heaters'),
+]
